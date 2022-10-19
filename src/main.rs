6eddef713a3db7b6309d9383fe9db1846179e094
morka17 <joshuamorka19@gmail.com>
2022-10-19 07:50:37 -0700

    created the customer struct with serde

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 
-
+mod models;
+mod db;
 
 
 fn main() {
--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,12 @@
 
+use serde::{Deserialize, Serialize};
 
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
 pub struct Customer {
-    
+    pub guid: String, 
+    pub first_name: String,
+    pub last_name: String,
+    pub email: String,
+    pub address: String,
 }
